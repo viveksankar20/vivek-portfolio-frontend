52adb38605e92ee74989dfd59b661b7dbfccfb2a
VivekPoongodi <viveksankar2010@gmail.com>
2025-05-13 08:35:54 +0530

    val

--- utils/isMobile.js
+++
@@ -1,5 +0,0 @@
-export const isMobile = () => {
-    if (typeof window === "undefined") return false; // Prevent errors during server-side rendering
-    return window.innerWidth <= 768; // You can adjust breakpoint if needed
-  };
-  
